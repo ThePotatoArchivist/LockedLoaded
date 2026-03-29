bb77feec5619a1836c04e524ca0ac70f32248f3b
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-29 00:31:38 -0700

    improve firework knockback

--- src/main/java/archives/tater/lockedloaded/mixin/enchantmenteffect/fireworkknockback/FireworkRocketEntityMixin.java
+++ src/main/java/archives/tater/lockedloaded/mixin/enchantmenteffect/fireworkknockback/FireworkRocketEntityMixin.java
@@ -26,9 +26,11 @@ public abstract class FireworkRocketEntityMixin extends Projectile {
             at = @At(value = "INVOKE:LAST", target = "Lnet/minecraft/world/entity/LivingEntity;hurtServer(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)Z")
     )
     private boolean fireworkKnockback(LivingEntity instance, ServerLevel level, DamageSource source, float amount, Operation<Boolean> original) {
+        var oldMovement = instance.getDeltaMovement();
+
         var result = original.call(instance, level, source, amount);
 
-        FireworkKnockbackKt.applyFireworkKnockback((FireworkRocketEntity) (Object) this, instance, 5.0);
+        FireworkKnockbackKt.applyFireworkKnockback((FireworkRocketEntity) (Object) this, instance, 5.0, oldMovement);
 
         return result;
     }
--- src/main/kotlin/archives/tater/lockedloaded/enchantment/FireworkKnockback.kt
+++ src/main/kotlin/archives/tater/lockedloaded/enchantment/FireworkKnockback.kt
@@ -4,13 +4,14 @@ import archives.tater.lockedloaded.registry.LockedLoadedAttachments
 import archives.tater.lockedloaded.util.*
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.projectile.FireworkRocketEntity
+import net.minecraft.world.phys.Vec3
 
-fun applyFireworkKnockback(firework: FireworkRocketEntity, entity: LivingEntity, radius: Double) {
+fun applyFireworkKnockback(firework: FireworkRocketEntity, entity: LivingEntity, radius: Double, oldMovement: Vec3) {
     if (entity != firework.owner || entity.onGround()) return
     val knockback = firework.getAttachedOrElse(LockedLoadedAttachments.FIREWORK_OWNER_KNOCKBACK, 0f);
     if (knockback <= 0) return
 
-    entity.deltaMovement = (entity.centerPos - firework.position()) * (knockback / radius)
+    entity.deltaMovement = oldMovement + (entity.centerPos - firework.position()) * (knockback / radius)
     entity.setIgnoreFallDamageFromCurrentImpulse(true, entity.position())
     entity.hurtMarked = true
     entity[LockedLoadedAttachments.DISCARD_FRICTION_CURRENT_IMPULSE] = McUnit.INSTANCE
